0360401dff542028a4e77b4c20166ccf53c888f2
riggsb2 <riggsb2@gmail.com>
2016-10-26 23:04:12 -0500

    Multi slot positioning
    change to numpy arrays to contain battlefield. Multiple slots for each
    battlefield position allowing up to 3v1 combat.

--- Battlecode.py
+++ Battlecode.py
@@ -1,24 +1,33 @@
 import random
 import copy
 import csv
+import numpy as np
 
+def attackroll(HitBon,DmgBon,DmgRng,CritRng,AC):
+    roll = random.randint(1,20)
+    attack = roll + HitBon
+    print('HitBon',HitBon)    
+    print('DmgBon',DmgBon)    
+    print('DmgRng',DmgRng)    
+    print('Critrng',CritRng)  
+    print('AC', AC)
+    print('Roll',roll)
+    print('Attack',attack)
 
-def battlelog(rnd_count):
-    if Ally_army[rnd_count][6]>=Enemy_army[rnd_count][2]:
-        print(Ally_army[rnd_count][0], ' Hit  Roll', Ally_army[rnd_count][6],Ally_army[rnd_count][7],'dmg. HP: ',Ally_army[rnd_count][1]) 
+    if attack >= AC:
+        if roll >= CritRng:
+            print('Crit!')            
+            Damage = random.randint(1,DmgRng)+random.randint(1,DmgRng)+DmgBon
+        else:
+            print('Hit')
+            Damage = random.randint(1,DmgRng)+DmgBon
     else:
-        print(Ally_army[rnd_count][0], ' Miss Roll', Ally_army[rnd_count][6],'HP: ',Ally_army[rnd_count][1])
+        print('Miss')
 
-    if Enemy_army[rnd_count][6]>=Ally_army[rnd_count][2]:
-        print(Enemy_army[rnd_count][0], ' Hit  Roll', Enemy_army[rnd_count][6],Enemy_army[rnd_count][7],'dmg. HP: ',Enemy_army[rnd_count][1]) 
-    else:
-        print(Enemy_army[rnd_count][0], 'Miss Roll', Enemy_army[rnd_count][6], 'HP: ',Enemy_army[rnd_count][1]) 
-        
-    print()
-    print('Allied units left ', len(Ally_army))
-    print('Enemy units left ', len(Enemy_army))
+        Damage = 0            
+    return(Damage)
 
-def battle(armyfile,Enemyfile,simulations):    
+def battle(armyfile,enemyfile,simulations):    
     Ewin = 0
     Awin = 0
     Dead = 0
@@ -28,11 +37,8 @@ def battle(armyfile,Enemyfile,simulations):
     Ally_stats = []
     Ally_army = []
     ##Inputs csv into array
-    with open(armyfile,'r') as csvfile:
-        data = csv.reader(csvfile, delimiter=',')
-        for row in data:
-            Ally_stats.append(row)
-    
+    Ally_stats = np.genfromtxt(armyfile, dtype = str, delimiter=',')
+
     ##Prints and assigns stats into Army Array
     for i in range(len(Ally_stats)):
         Ally = []
@@ -56,10 +62,7 @@ def battle(armyfile,Enemyfile,simulations):
     Enemy_army = []
     
     ##Inputs csv into array
-    with open(Enemyfile,'r') as csvfile:
-        data = csv.reader(csvfile, delimiter=',')
-        for row in data:
-            Enemy_stats.append(row)
+    Enemy_stats = np.genfromtxt(enemyfile, dtype = str, delimiter=',')
     
     ##Prints and assigns stats into Army Array
     for i in range(len(Enemy_stats)):
@@ -81,19 +84,8 @@ def battle(armyfile,Enemyfile,simulations):
     
     #Runs several simulations
     for run in range(simulations):
-        #Reads ally army from CSV file and prints on screen
-        #armyfile ='Alley_army.csv'
-        #Ally_stats = []
         Ally_army = []
-        #print(Ally_stats)
-        ##Inputs csv into array
-        #with open(armyfile,'r') as csvfile:
-         #   data = csv.reader(csvfile, delimiter=',')
-          #  for row in data:
-           #     Ally_stats.append(row)
-            #print()
-        
-        ##Prints and assigns stats into Army Array
+        ##assigns stats into Army Array
         for i in range(len(Ally_stats)):
             Ally = []
             for j in range(len(Ally_stats[i])):
@@ -103,24 +95,13 @@ def battle(armyfile,Enemyfile,simulations):
                     units = Ally_stats[i][j] #sets number of units
                 else:
                     Ally.append(Ally_stats[i][j]) #assembles Alley stat block
+            Ally.append('A')
             if i != 0:
                 for k in range(int(units)):
                     Ally_army.append(copy.deepcopy(Ally))
-            #print() 
         
-        #Reads Enemy army from CSV file and prints on screen
-        #Enemyfile ='Enemy_army.csv'
-        #Enemy_stats = []
-        Enemy_army = []
-
-        ##Inputs csv into array
-        #with open(Enemyfile,'r') as csvfile:
-        #    data = csv.reader(csvfile, delimiter=',')
-        #    for row in data:
-        #        Enemy_stats.append(row)
-            #print()
-        
-        ##Prints and assigns stats into Army Array
+        Enemy_army = []      
+        #assigns stats into Army Array
         for i in range(len(Enemy_stats)):
             Enemy = []
             for j in range(len(Enemy_stats[i])):
@@ -130,10 +111,10 @@ def battle(armyfile,Enemyfile,simulations):
                     units = Enemy_stats[i][j] #sets number of units
                 else:
                     Enemy.append(Enemy_stats[i][j]) #assembles Alley stat block
+            Enemy.append('E')
             if i != 0:
                 for k in range(int(units)):
                     Enemy_army.append(copy.deepcopy(Enemy))
-            #print()
         
         #Randomizes Unit Positions
         for i in range(1,len(Ally_army)):
@@ -146,40 +127,70 @@ def battle(armyfile,Enemyfile,simulations):
             Pos2 = random.randint(0,len(Enemy_army)-1)
             Enemy_army[Pos1], Enemy_army[Pos2] = Enemy_army[Pos2], Enemy_army[Pos1]
         
-        rnd=1
+        #Issue with interpretation of values in 3D array              
+        Battlefield = np.empty([max(len(Ally_army),len(Enemy_army)),
+                                4,
+                                len(Ally)],
+                                dtype='a2')
+        print(Battlefield)        
+        #Battlefield[:,0] = Ally_army[:]
+        #print(Battlefield)        
+        #Battlefield[0:len(Enemy_army),1] = Enemy_army[:]
+        #print(Battlefield)        
+
         
+        rnd=1      
         #Run the battle. As long as one army doesn't overwhelm the other (5x size), continue fighting
         #while len(Ally_army)<5*len(Enemy_army) and len(Enemy_army)<5*len(Ally_army):
         while len(Ally_army)>0 and len(Enemy_army)>0: #TO THE DEATH
-            #print('********************* Round, ',rnd,' *********************')
-            rnd_length=(min(len(Ally_army),len(Enemy_army)))
-            #print('Round is ', rnd_length, ' turns.')
-        
+            rnd_length=(len(Battlefield))
             #loops through entire army for 1v1 combat
             for rnd_count in range(rnd_length):
-        
-                #print(rnd_count+1)
-                #print()
-                #Assigned to hit and damage for each active unit
-                Ally_army[rnd_count][6] = random.randint(1,20)+int(Ally_army[rnd_count][3])
-                Ally_army[rnd_count][7] = random.randint(1,int(Ally_army[rnd_count][5]))+int(Ally_army[rnd_count][4])
-                Enemy_army[rnd_count][6] = random.randint(1,20)+ int(Enemy_army[rnd_count][3])
-                Enemy_army[rnd_count][7] = random.randint(1,int(Enemy_army[rnd_count][5]))+ int(Enemy_army[rnd_count][4])
-        
-                #Checks to see if they hit their opponent
-                if Ally_army[rnd_count][6]>=int(Enemy_army[rnd_count][2]):
-                    Enemy_army[rnd_count][1]-=Ally_army[rnd_count][7]
-        
-                if Enemy_army[rnd_count][6]>=Ally_army[rnd_count][2]:
-                    Ally_army[rnd_count][1]-=Enemy_army[rnd_count][7]
+                #Check who is in what slot of a position
+                Ally_pos = []
+                Enemy_pos = []                   
+                for Slot in range(len(Battlefield[rnd_count])):
+                    if Battlefield[rnd_count][Slot][9] ==b'A':
+                        Ally_pos.append(Slot)
+                    elif Battlefield[rnd_count][Slot][9] ==b'E':
+                        Enemy_pos.append(Slot)
+                
+                for AT in range(len(Ally_pos)):
+                    if Enemy_pos:   #Checks to see if there are enemies to fight                 
+                        print('Ally Attack')                    
+                        dmg = attackroll(int(Battlefield[rnd_count][Ally_pos[AT]][3]),#Hit Bonus
+                                      int(Battlefield[rnd_count][Ally_pos[AT]][4]),#Dmg Bonus
+                                      int(Battlefield[rnd_count][Ally_pos[AT]][5]),#Dmg Range
+                                      int(Battlefield[rnd_count][Ally_pos[AT]][6]),#Crit Range
+                                      int(Battlefield[rnd_count][Enemy_pos[0]][2]))#Enemy AC
+                        print(dmg)  
+                        HP = int(Battlefield[rnd_count][Enemy_pos[0]][1])
+                        HP -= dmg
+                        Battlefield[rnd_count][Enemy_pos[0]][1] = HP
+                        print(Battlefield[rnd_count][Enemy_pos[0]][1])
+                        print()
+                
+                for ET in range(len(Enemy_pos)):
+                    if Ally_pos:  #Checks to see if there are allies to fight                  
+                        print('Enemy Attack')                    
+                        dmg = attackroll(int(Battlefield[rnd_count][Enemy_pos[ET]][3]),#Hit Bonus
+                                      int(Battlefield[rnd_count][Enemy_pos[ET]][4]),#Dmg Bonus
+                                      int(Battlefield[rnd_count][Enemy_pos[ET]][5]),#Dmg Range
+                                      int(Battlefield[rnd_count][Enemy_pos[ET]][6]),#Crit Range
+                                      int(Battlefield[rnd_count][Ally_pos[0]][2]))#Enemy AC
+                        print(dmg)  
+                        HP = int(Battlefield[rnd_count][Ally_pos[0]][1])
+                        HP -= dmg
+                        Battlefield[rnd_count][Ally_pos[0]][1] = HP
+                        print(Battlefield[rnd_count][Ally_pos[0]][1])
+                        print()
             
             #Check for dead bodies
             for rnd_count in range(rnd_length):
-                if Ally_army[rnd_count][1] <=0:
-                    Ally_army[rnd_count] = []
-                if Enemy_army[rnd_count][1] <=0:
-                    Enemy_army[rnd_count] = []
-        
+                for Slot in range(len(Battlefield[rnd_count])):
+                    if int(Battlefield[rnd_count][Slot][1]) <=0:
+                        print('Dead')                        
+
             #remove dead bodies from the field        
             Ally_army = [x for x in Ally_army if x]
             Enemy_army = [x for x in Enemy_army if x]
@@ -202,4 +213,5 @@ def battle(armyfile,Enemyfile,simulations):
     #print('Enemy wins: ', Ewin)
     #print('Complete destruction: ',Dead)
 
-#print('Allies win: ', battle('Party.csv','Blaze Boa.csv',1000),'%')       
+#print('Allies win: ', battle('Party.csv','Blaze Boa.csv',1000),'%')  
+battle('Party.csv','Blaze Boa.csv',1)     
