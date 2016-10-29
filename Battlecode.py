e2ff3d940ad196b430106bcd73667d92089d7f73
riggsb2 <riggsb2@gmail.com>
2016-10-29 17:36:03 -0500

    Nv1 combat complete
    Finished Nv1 combat. Current allows 3v1.
    -Fighters will shift to empty slots if there are no enemies to fit
    -Empty rows of 3D array 'Battefield' removed after each round

--- Battlecode.py
+++ Battlecode.py
@@ -1,28 +1,27 @@
 import random
 import copy
-import csv
 import numpy as np
 
 def attackroll(HitBon,DmgBon,DmgRng,CritRng,AC):
     roll = random.randint(1,20)
     attack = roll + HitBon
-    print('HitBon',HitBon)    
-    print('DmgBon',DmgBon)    
-    print('DmgRng',DmgRng)    
-    print('Critrng',CritRng)  
-    print('AC', AC)
-    print('Roll',roll)
-    print('Attack',attack)
+    #print('HitBon',HitBon)    
+    #print('DmgBon',DmgBon)    
+    #print('DmgRng',DmgRng)    
+    #print('Critrng',CritRng)  
+    #print('AC', AC)
+    #print('Roll',roll)
+    #print('Attack',attack)
 
     if attack >= AC:
         if roll >= CritRng:
-            print('Crit!')            
+            #print('Crit!')            
             Damage = random.randint(1,DmgRng)+random.randint(1,DmgRng)+DmgBon
         else:
-            print('Hit')
+            #print('Hit')
             Damage = random.randint(1,DmgRng)+DmgBon
     else:
-        print('Miss')
+        #print('Miss')
 
         Damage = 0            
     return(Damage)
@@ -128,25 +127,27 @@ def battle(armyfile,enemyfile,simulations):
             Enemy_army[Pos1], Enemy_army[Pos2] = Enemy_army[Pos2], Enemy_army[Pos1]
         
         #Issue with interpretation of values in 3D array              
-        Battlefield = np.empty([max(len(Ally_army),len(Enemy_army)),
+        Battlefield = np.zeros([max(len(Ally_army),len(Enemy_army)),
                                 4,
                                 len(Ally)],
                                 dtype='a2')
-        print(Battlefield)        
-        #Battlefield[:,0] = Ally_army[:]
-        #print(Battlefield)        
-        #Battlefield[0:len(Enemy_army),1] = Enemy_army[:]
-        #print(Battlefield)        
+        Battlefield[0:len(Ally_army),0] = Ally_army[:]        
+        Battlefield[0:len(Enemy_army),1] = Enemy_army[:]
 
-        
         rnd=1      
         #Run the battle. As long as one army doesn't overwhelm the other (5x size), continue fighting
         #while len(Ally_army)<5*len(Enemy_army) and len(Enemy_army)<5*len(Ally_army):
         while len(Ally_army)>0 and len(Enemy_army)>0: #TO THE DEATH
+            #print(len(Ally_army))
+            #print(len(Enemy_army))
             rnd_length=(len(Battlefield))
-            #loops through entire army for 1v1 combat
+            #loops through entire battlefiled
+                        
+            #print(len(Battlefield))
+            #print(Battlefield)            
             for rnd_count in range(rnd_length):
                 #Check who is in what slot of a position
+                #print('Turn', rnd_count)                
                 Ally_pos = []
                 Enemy_pos = []                   
                 for Slot in range(len(Battlefield[rnd_count])):
@@ -157,46 +158,82 @@ def battle(armyfile,enemyfile,simulations):
                 
                 for AT in range(len(Ally_pos)):
                     if Enemy_pos:   #Checks to see if there are enemies to fight                 
-                        print('Ally Attack')                    
+                        #print('Ally Attack')                    
                         dmg = attackroll(int(Battlefield[rnd_count][Ally_pos[AT]][3]),#Hit Bonus
                                       int(Battlefield[rnd_count][Ally_pos[AT]][4]),#Dmg Bonus
                                       int(Battlefield[rnd_count][Ally_pos[AT]][5]),#Dmg Range
                                       int(Battlefield[rnd_count][Ally_pos[AT]][6]),#Crit Range
                                       int(Battlefield[rnd_count][Enemy_pos[0]][2]))#Enemy AC
-                        print(dmg)  
+                        #print(dmg)  
                         HP = int(Battlefield[rnd_count][Enemy_pos[0]][1])
                         HP -= dmg
                         Battlefield[rnd_count][Enemy_pos[0]][1] = HP
-                        print(Battlefield[rnd_count][Enemy_pos[0]][1])
-                        print()
+                        #print(Battlefield[rnd_count][Enemy_pos[0]][1])
+                        #print()
+                    elif not Enemy_pos:
+                        #print('No Enemies left')
+                        for Slot in range(len(Battlefield[rnd_count])): #Check slots for ally
+                            for opening in range(len(Battlefield[rnd_count-1])): #Finds opening in position above
+                                if Battlefield[rnd_count-1][opening][0] == b'': #if there's an opening, fill it
+                                    #print('Empty slot')
+                                    Battlefield[rnd_count-1][opening] = Battlefield[rnd_count][Slot]
+                                    Battlefield[rnd_count][Slot]=['','','','','','','','','','']                
                 
                 for ET in range(len(Enemy_pos)):
                     if Ally_pos:  #Checks to see if there are allies to fight                  
-                        print('Enemy Attack')                    
+                        #print('Enemy Attack')                    
                         dmg = attackroll(int(Battlefield[rnd_count][Enemy_pos[ET]][3]),#Hit Bonus
                                       int(Battlefield[rnd_count][Enemy_pos[ET]][4]),#Dmg Bonus
                                       int(Battlefield[rnd_count][Enemy_pos[ET]][5]),#Dmg Range
                                       int(Battlefield[rnd_count][Enemy_pos[ET]][6]),#Crit Range
                                       int(Battlefield[rnd_count][Ally_pos[0]][2]))#Enemy AC
-                        print(dmg)  
+                        #print(dmg)  
                         HP = int(Battlefield[rnd_count][Ally_pos[0]][1])
                         HP -= dmg
                         Battlefield[rnd_count][Ally_pos[0]][1] = HP
-                        print(Battlefield[rnd_count][Ally_pos[0]][1])
-                        print()
-            
+                        #print(Battlefield[rnd_count][Ally_pos[0]][1])
+                        #print()
+                    elif not Ally_pos:
+                        #print('No Allies left')
+                        for Slot in range(len(Battlefield[rnd_count])): #Check slots for ally
+                            for opening in range(len(Battlefield[rnd_count-1])): #Finds opening in position above
+                                if Battlefield[rnd_count-1][opening][0] == b'': #if there's an opening, fill it
+                                    #print('Empty slot')
+                                    Battlefield[rnd_count-1][opening] = Battlefield[rnd_count][Slot]
+                                    Battlefield[rnd_count][Slot]=['','','','','','','','','','']
+               
+                
+            #print('Death Check')            
             #Check for dead bodies
             for rnd_count in range(rnd_length):
                 for Slot in range(len(Battlefield[rnd_count])):
-                    if int(Battlefield[rnd_count][Slot][1]) <=0:
-                        print('Dead')                        
-
+                    #print('Deathcheck',rnd_count,Slot)                    
+                    #if Battlefield[rnd_count][Slot][0] == b'0':
+                        #break
+                    if Battlefield[rnd_count][Slot][1] <=b'0':
+                        if Battlefield[rnd_count][Slot][9] ==b'A':
+                            Ally_army[rnd_count] = []
+                        elif Battlefield[rnd_count][Slot][9] ==b'E':
+                            Enemy_army[rnd_count] = []                        
+                        #print('Dead', rnd_count, Slot, Battlefield[rnd_count][Slot])                        
+                        Battlefield[rnd_count][Slot]=['','','','','','','','','','']
+                                    
+            
+            #print('Remove bodies')
             #remove dead bodies from the field        
             Ally_army = [x for x in Ally_army if x]
             Enemy_army = [x for x in Enemy_army if x]
-        
-            rnd+=1
             
+            #clear empty blocks of array
+            #print('Clean up battfield array')
+            for i in reversed(range(len(Battlefield))):
+                #print(i)
+                #print(np.count_nonzero(Battlefield[i]))
+                if np.count_nonzero(Battlefield[i]) == 0:
+                    Battlefield = np.delete(Battlefield,(i),axis = 0)
+            rnd+=1
+        
+        #print('Win count')
         if len(Ally_army)<5*len(Enemy_army):
             #print('The Enemy army wins. Allies have ',len(Ally_army),' unit(s) remaining')
             Ewin +=1
@@ -206,7 +243,7 @@ def battle(armyfile,enemyfile,simulations):
         else:
             #print('Everyone is dead.')
             Dead+=1
-    
+    #print(Battlefield)
     WinRatio = round((Awin/simulations)*100)
     return(WinRatio)
     #print('Ally wins: ',Awin)
@@ -214,4 +251,4 @@ def battle(armyfile,enemyfile,simulations):
     #print('Complete destruction: ',Dead)
 
 #print('Allies win: ', battle('Party.csv','Blaze Boa.csv',1000),'%')  
-battle('Party.csv','Blaze Boa.csv',1)     
+print(battle('Party.csv','Blaze Boa.csv',1000))
